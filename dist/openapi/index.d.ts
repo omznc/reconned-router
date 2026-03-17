bc15e514566f70411e32f8e1afbd3ce5d09e43bb
Omar Zunic <git@omarzunic.com>
2026-03-18 00:27:13 +0100

    chore: removed node modules

--- dist/openapi/index.d.ts
+++
@@ -1,9 +0,0 @@
-import type { Router } from "../router";
-import type { OpenAPIOptions, OpenAPISpec } from "../types";
-export declare function generateOpenAPISpec(baseUrl: string, routers: Router[], options?: OpenAPIOptions): Promise<OpenAPISpec>;
-export declare function createOpenAPIHandler(routers: Router[], options?: OpenAPIOptions): {
-    getSpec(baseUrl: string): Promise<OpenAPISpec>;
-    handleSpec(request: Request): Promise<Response>;
-    handleDocs(scalarJsUrl?: string): Response;
-};
-//# sourceMappingURL=index.d.ts.map
